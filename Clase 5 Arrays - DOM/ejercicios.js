8ecf0ee8a180a992b97db2cd74ae025ad0ac6a49
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-03-21 14:16:56 -0300

    agrego ejercicios hasta el 13

--- Clase 5 Arrays - DOM/ejercicios.js
+++ Clase 5 Arrays - DOM/ejercicios.js
@@ -55,6 +55,7 @@ Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con u
 medio. Realizar la misma actividad, pero ahora almacenando los valores en un array y
 luego recorriendolo para mostrar el resultado deseado del ejercicio.) */
 
+
 let listaTextos = ""
 
 while (true) {
@@ -86,7 +87,7 @@ while (true) {
     } else {
         listaTextos1.push(cadenaDeTexto1)
     }
-} 
+}
 
 /* 9. Hacer un formulario con dos campos (pesos y dólares) que al presionar un botón
 convierte de dólares a pesos. Supondremos que un dólar son trescientos pesos.
@@ -98,3 +99,95 @@ function cambioDolaresPesos () {
     const pesos = dolar * cambio
     document.getElementById("pesos").value = pesos
 }
+
+/* 10. Generar la inversa del ejercicio anterior (de pesos a dólares). */
+
+function cambioPesosDolares () {
+    const pesos = document.getElementById("peso").value
+    const cambio = 300
+    const dolar = pesos / cambio
+    document.getElementById("dolar").value = dolar
+}
+
+/* 11. Hacer un formulario que convierta de grados centígrados a grados Fahrenheit. Para
+ello deberá multiplicar por 9/5 y sumar 32. Tener en cuenta que 30 grados centígrados
+son 86 grados fahrenheit. */
+
+function convertirGradosCentigradosFahrenheit () {
+    const centigrados = document.getElementById("cent").value
+    const cambio = (centigrados * 9/5) + 32
+    const fahrenheit = cambio
+    document.getElementById("fahr").value = fahrenheit
+}
+
+/* 12. Hacer un formulario para solicitar el envío de una caja de cartón. En el formulario
+podremos especificar las características de la caja. */
+
+document.getElementById('texto-envio').onclick = function() {
+
+    const nombre = document.getElementById("nombre").value
+    const material = document.getElementById("material").value
+
+    if (document.getElementById("dimensiones1").checked) {
+        dimensiones = document.getElementById("dimensiones1").value
+    } else if (document.getElementById("dimensiones2").checked) {
+        dimensiones = document.getElementById("dimensiones2").value
+    } else {
+        dimensiones = document.getElementById("dimensiones3").value
+    }
+
+    const comentarios = document.getElementById("comentarios").value
+
+    document.getElementById("texto-junto").innerHTML = `El envío será a nombre de ${nombre[0].toUpperCase()+nombre.slice(1)}, en una caja de ${material} ${dimensiones}. Comentarios: ${comentarios}`
+}
+
+/* 13. A través de un formulario, el usuario debe introducir un número secreto que estará
+entre 0 y 5. El usuario tendrá tres intentos para acertar el número, si no lo logra, habrá perdido el
+juego. Debemos indicar en un cuadro de texto si el usuario gana, falla el número o pierde. */
+
+function adivinarNumero () {
+    let contador = 3
+    const numero = 2
+    do {
+        numero1 = parseInt(prompt("Ingrese un número entre el 0 y el 5: ", document.getElementById("numero1").value));
+        contador--
+        if (numero1 < 0 || numero1 >5) {
+            alert(`El número ingresado debe estar entre el 0 y el 5. Te quedan ${contador} oportunidades`)
+        } else if (numero1 != numero && contador > 0) {
+            alert(`El número ingresado no es el correcto. Te quedan ${contador} oportunidades`)
+        }
+    } while (contador > 0 && numero1 != numero)
+
+    if (numero1 === numero && contador <= 3) {
+        alert(`El número ${numero1} era el correcto!`)
+    } else {
+        alert("Perdiste las 3 oportunidades.")
+    }
+}
+
+/* 
+document.getElementById('boton-numero').onclick = function adivinarNumero () {
+    let contador = 3
+    const numero = 2
+    do {
+        const numero1 = document.getElementById("numero1").value
+        contador--
+        if (numero1 < 0 || numero1 > 5) {
+            document.getElementById('mensaje-error').innerHTML = `El número ingresado debe estar entre el 0 y el 5. Te quedan ${contador} oportunidades`
+        } else if (numero1 != numero && contador > 0) {
+            document.getElementById('mensaje-error').innerHTML = `El número ingresado no es el correcto. Te quedan ${contador} oportunidades`
+        }
+    } while (contador > 0 && numero1 != numero)
+
+    if (numero1 === numero && contador <= 3) {
+        document.getElementById('mensaje-correcto').innerHTML = `El número ${numero1} era el correcto!`
+    } else {
+        document.getElementById('mensaje-error').innerHTML = "Perdiste las 3 oportunidades."
+    }
+}
+
+adivinarNumero()
+*/
+
+/* 14. Dado un input y un botón, escribir nombres de personas e ir mostrando en una lista
+desordenada a medida que se agregan. */
