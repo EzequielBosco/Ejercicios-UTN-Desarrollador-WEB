63a4303f28695271e40c4886df6c9c7a90ed9a83
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-03-29 21:13:18 -0300

    agrego clase 10 jQuery

---
+++ Clase 10 jQuery/prueba.js
@@ -0,0 +1,9 @@
+function miFuncion(){
+    setTimeout(otraFuncion,3000)
+};
+
+/* a los 3 segundos se activa el alert */
+function otraFuncion(){
+    alert("Somos necesarios, un fragmento de la fatalidad; formamos parte del todo, somos en el todo; no hay nada que pueda juzgar, medir, comparar y condenar nuestra    existencia, pues ello equivaldría a juzgar, medir, comparar y condenar el todo. Friedrich Nietzsche");
+};
+miFuncion(); 
