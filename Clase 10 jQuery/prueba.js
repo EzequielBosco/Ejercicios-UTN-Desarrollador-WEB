cbbb4900cdbb9e42965071437d4e35dd0f0beea2
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-04-06 15:18:34 -0300

    empiezo ejercicios clase 10 jQuery

--- Clase 10 jQuery/prueba.js
+++ Clase 10 jQuery/prueba.js
@@ -1,9 +1,89 @@
-function miFuncion(){
-    setTimeout(otraFuncion,3000)
-};
+// 1. Genera un array de números aleatorios, ordenarlos y mostrarlo en una lista
+// desordenada. Tiene que crear la lista uno mismo.
 
-/* a los 3 segundos se activa el alert */
-function otraFuncion(){
-    alert("Somos necesarios, un fragmento de la fatalidad; formamos parte del todo, somos en el todo; no hay nada que pueda juzgar, medir, comparar y condenar nuestra    existencia, pues ello equivaldría a juzgar, medir, comparar y condenar el todo. Friedrich Nietzsche");
-};
-miFuncion(); 
+let array = []
+
+function numeroRandom() {
+    return parseInt(Math.random() * (200000 - 0) + 0);
+}
+
+for (let i = 0; i <= 10; i++) {
+    array.push(numeroRandom())
+}
+
+const arrayOrdenado = array.sort(function(a, b) {return a - b})
+
+let crearLista = document.getElementById("crear-lista")
+crearLista.addEventListener("click", crearListaDesordenada)
+
+function crearListaDesordenada(){
+    document.getElementById("texto").innerHTML = "<h4>Lista:</h4>"
+    document.getElementById("fin").innerHTML = "<h5>Fín</h4>"
+    document.createElement("ul");
+    for (let i = 0; i < arrayOrdenado.length; i++) {
+        document.getElementById("lista").innerHTML += "<li>" + arrayOrdenado[i];
+    }
+    crearLista.removeEventListener("click", crearListaDesordenada);
+}
+
+// 2. Dado el array = [1,2,3,4,5,6]
+// a. Iterar por todos los elementos dentro de un array utilizando while y mostrarlos
+// en pantalla.
+// b. Iterar por todos los elementos dentro de un array utilizando for y mostrarlos en
+// pantalla.
+// c. Iterar por todos los elementos dentro de un array utilizando .forEach y
+// mostrarlos en pantalla.
+// d. Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
+// e. Generar una copia de un array pero con todos los elementos incrementados en 1.
+// f. Calcular el promedio
+
+array1 = [1,2,3,4,5,6]
+
+// a
+console.log("Ejercicio A")
+
+b = 0
+while (b < array1.length) {
+    console.log(array1[b])
+    b += 1
+}
+
+// b
+console.log("Ejercicio B")
+
+for (let i = 0; i < array1.length; i++) {
+    console.log(array1[i])
+}
+
+// c
+console.log("Ejercicio C")
+
+array1.forEach(item => {
+    console.log(item)
+});
+
+// d
+console.log("Ejercicio D")
+
+array1.forEach(item => {
+    console.log(item + 1)
+});
+
+// e
+console.log("Ejercicio E")
+
+let array2 = array1.map(item => {
+    return(item + 1)
+});
+console.log(array2)
+
+// f
+console.log("Ejercicio F")
+
+sumaNotas = array2.reduce((acumulador, elemento) => acumulador + elemento, 0)
+
+function calcularPromedio() {
+    let promedio = sumaNotas / array2.length;
+    console.log(promedio)
+}
+calcularPromedio()
